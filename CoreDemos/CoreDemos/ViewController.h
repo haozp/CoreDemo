f369073e8cda518e5c703f3c8a5496c7be7eefb2
haozp <leo@leodemacbook-pro.local>
2016-09-12 16:49:54 +0800

    初始化CoreDemos工程

---
+++ CoreDemos/CoreDemos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDemos
+//
+//  Created by leo on 16/9/12.
+//  Copyright © 2016年 haozp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDemos/CoreDemos/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  CoreDemos
+//
+//  Created by leo on 16/9/12.
+//  Copyright © 2016年 haozp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor whiteColor];
+    NSLog(@"处理一些问题");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
